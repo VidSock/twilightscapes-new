3b468ea1acc5e2009d90c052cc9c79fe8cfbd789
twilightscapes <ihatespam@gmail.com>
2019-12-29 00:40:47 -0600

    remove font loader

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,7 +121,7 @@ module.exports = {
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
         enableIdentityWidget: true,
-        htmlTitle: `Gatsby Starter Business Content Manager`,
+        htmlTitle: `Twilightscapes Content Manager`,
       },
     },
 
